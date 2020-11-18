1069161fb70a1b5a73512bc53d485c2daa80a734
lushpurr <lushpurr@gmail.com>
2020-11-18 17:21:24 +0000

    initial commit

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const startPoints = this.journeys.map( (journey) => {
+    return journey.startLocation;
+  });
+return startPoints;
+};
+
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const endPoints = this.journeys.map( (journey) => {
+    return journey.endLocation;
+  });
+  return endPoints;
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
